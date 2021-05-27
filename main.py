32a1c40ee7218dec2f1f008aeb59858c10eb1d0d
Serg <fsu_50@mail.ru>
2021-05-27 20:18:50 +0300

    Файлы домашнего задания

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle
+
+player = turtle.Turtle()
+player.shape("turtle")
+player.forward(100)
+player.left(90)
+player.fd(100)
+player.lt(90)
+player.up()
+player.down()
+player.rt(90)
+
+turtle.done()
